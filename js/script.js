c8263d6ebb7221413ae6df693469528e98e53cc8
Abhay Sharma <abhaysharmawebd@gmail.com>
2023-06-09 10:09:18 -0400

    adds demo for methods

--- js/script.js
+++ js/script.js
@@ -9,3 +9,42 @@ messageList.children[0].innerHTML += myColors[3]
 myColors[4] = "cyan"
 messageList.children[1].innerHTML += myColors[4]
 
+// adding a value at the end of array using push()
+myColors.push("darksalmon")
+
+messageList.children[2].innerHTML += myColors
+
+// remove a value from the end using pop()
+myColors.pop()
+
+messageList.children[3].innerHTML += myColors
+
+// remove one item from the start using shift()
+myColors.shift()
+messageList.children[4].innerHTML += myColors
+
+// adding a value at the start of the array using unshift()
+myColors.unshift("hotpink")
+messageList.children[5].innerHTML += myColors
+
+// splice() accepts three params index to start from, how many items to delete, what values to add 
+myColors.splice(2, 0, "royalblue")
+messageList.children[6].innerHTML += myColors
+
+// sorting the array using sort()
+myColors.sort()
+messageList.children[7].innerHTML += myColors
+
+// Concatenating two arrays
+const darkColors = ["darkseagreen", "darkgreen", "darkred"]
+
+// Using concat method
+// const allColors = myColors.concat(darkColors)
+
+// another way of concatenating the arrays
+const allColors = [...myColors, ...darkColors]
+
+// messageList.children[8].innerHTML += allColors
+
+// using the join() method to join the values in a string
+messageList.children[8].innerHTML += allColors.join(" - ")
